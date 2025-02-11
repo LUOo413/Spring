48841c20b477021ef35dd3496c780c8090212a36
luo <minnie3347719@gmail.com>
2025-02-11 11:14:20 +0800

    no message

---
+++ workspace/DemoSpringMvcProject/src/main/java/tw/demospringmvcproject/DemoSpringMvcProjectApplication.java
@@ -0,0 +1,13 @@
+package tw.demospringmvcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringMvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringMvcProjectApplication.class, args);
+	}
+
+}
---
+++ workspace/DemoSpringMvcProject/src/test/java/tw/demospringmvcproject/DemoSpringMvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.demospringmvcproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringMvcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
